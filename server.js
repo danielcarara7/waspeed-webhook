319361bfa8572d7c9d23ed7a859c8a0e84a779f1
danielcarara7 <danielcarara7@gmail.com>
2025-10-30 17:14:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const { google } = require('googleapis');
 const app = express();
 
 app.use(express.json());
+app.use(express.json());
+
+// Adiciona isso aqui:
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 // Configurações
 const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
